3ebf896af7a4d31a49a783b6c8d56f7c15638e94
James <>
2021-01-23 23:37:45 -0800

    Add helmet package

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const members = require('../routes/members');
 const notFound = require("../routes/notFound");
+const helmet = require("helmet");
 const error = require("../middleware/error");
 
 module.exports = app => {
+  app.use(helmet());
   app.use("/api/members", members);
   app.use("/", notFound);
   app.use(error);
