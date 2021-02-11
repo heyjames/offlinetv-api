457a9cf4dd712223ad2606f7b47fbf07cc7b04d1
James <>
2021-02-11 15:59:47 -0800

    Revert "Add redis"
    This reverts commit 1c0a7e1a7b374b4201d9d921acad1f05658043b4.

--- services/memberService.js
+++ services/memberService.js
@@ -1,14 +1,10 @@
 const fs = require('fs').promises;
 const filePath = "./data/live.json";
-const { redis } = require('../src/redis');
-const JSONCache = require('redis-json');
-
-const jsonCache = new JSONCache(redis, {prefix: "cache:"});
 
 // Load members JSON from file system.
 async function loadMembers() {
   try {
-    console.log("Loading members from JSON file...");
+    console.log("Loading members...");
     const file = await fs.readFile(filePath);
     const { data: members } = JSON.parse(file);
     return members;
@@ -18,30 +14,10 @@ async function loadMembers() {
   }
 }
 
-async function getMembersFromRedis() {
-  try {
-    console.log("Serving members from Redis...");
-    const redisMembers = await jsonCache.get('redisMembers')
-    return redisMembers;
-  } catch (error) {
-    console.error("Failed to serve members from Redis.");
-    process.exit(1);
-  }
-}
-
-async function saveMembersToRedis(members) {
-  try {
-    const liveObject= { errors: [], data: members };
-    await jsonCache.rewrite('redisMembers', liveObject);
-  } catch (error) {
-    console.error("Failed to save members to Redis.");
-  }
-}
-
 // Write members as JSON to file system.
 async function saveMembers(members) {
   try {
-    console.log("Saving members to JSON file...");
+    console.log("Saving members...");
     // TODO: Handle the errors array properly.
     const liveObject= { errors: [], data: members };
     const LiveJSON = JSON.stringify(liveObject, null, 2);
@@ -54,7 +30,5 @@ async function saveMembers(members) {
 
 module.exports = {
   loadMembers,
-  saveMembers,
-  saveMembersToRedis,
-  getMembersFromRedis
+  saveMembers
 };
